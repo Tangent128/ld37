56e1babc56c65097109c92afde5a04bf8e9a0075
Tangent 128 <Tangent128@gmail.com>
2016-12-11 01:02:26 -0500

    Time Machine shuffles between periods

--- escape/scenario.ts
+++ escape/scenario.ts
@@ -2,8 +2,24 @@
 /// <reference path="./slide.ts" />
 /// <reference path="./gui-room.ts" />
 
+enum TimePeriod {
+    Alchemy,
+    Present,
+    Future
+};
 class GameState {
+
+    // time period
+    TimePeriod = TimePeriod.Present;
+
+    // past
+
+    // present
     Desk = new Array<InventoryItemType>();
+
+    // future
+
+    // misc
     Inventory = new Array<InventoryItemType>();
 
     // permanent objects
@@ -81,6 +97,9 @@ function ToObjectLayer(room: GuiRoom<GameState>, entity: any) {
 };
 
 function PopupTimeMachine(room: GuiRoom<GameState>) {
+
+    let cancel: Function;
+
     let root = room.makeDummyObject("#40a", 750, 150, "Time Machine");
     ToObjectLayer(room, root);
     Slide.Slide(root, 200, 250, 40);
@@ -89,34 +108,59 @@ function PopupTimeMachine(room: GuiRoom<GameState>) {
     let alchemy = room.makeDummyObject("#880", 0, 0, "Alchemy");
     ToObjectLayer(room, alchemy);
     Pin.Attach(root, alchemy, 150, -70);
+    room.onClick(alchemy, clickedBy => {
+        if(clickedBy == null) {
+            room.State.TimePeriod = TimePeriod.Alchemy;
+            GenerateRoom(room);
+            cancel();
+        }
+    });
 
     let present = room.makeDummyObject("#fcc", 0, 0, "Present");
     ToObjectLayer(room, present);
     Pin.Attach(root, present, 150, 0);
+    room.onClick(present, clickedBy => {
+        if(clickedBy == null) {
+            room.State.TimePeriod = TimePeriod.Present;
+            GenerateRoom(room);
+            cancel();
+        }
+    });
 
     let future = room.makeDummyObject("#0fa", 0, 0, "Future");
     ToObjectLayer(room, future);
     Pin.Attach(root, future, 150, 70);
+    room.onClick(future, clickedBy => {
+        if(clickedBy == null) {
+            room.State.TimePeriod = TimePeriod.Future;
+            GenerateRoom(room);
+            cancel();
+        }
+    });
 
     let back = room.makeDummyObject("#440", 0, 0, "Cancel");
     ToObjectLayer(room, back);
     Pin.Attach(root, back, 70, 70);
     room.onClick(back, clickedBy => {
         if(clickedBy == null) {
-            Slide.Slide(root, -250, 250, 40, () => {
-                root.deleted = true;
-                alchemy.deleted = true;
-                present.deleted = true;
-                future.deleted = true;
-                back.deleted = true;
-            });
+            cancel();
         }
     });
+
+    cancel = () => {
+        Slide.Slide(root, -250, 250, 40, () => {
+            root.deleted = true;
+            alchemy.deleted = true;
+            present.deleted = true;
+            future.deleted = true;
+            back.deleted = true;
+        });
+    };
 };
 
 function GenerateRoom(room: GuiRoom<GameState>) {
 
-    let state = room.State;
+    let State = room.State;
 
     // remove entities to rebuild
     room.Entities.map(entity => {
@@ -125,11 +169,22 @@ function GenerateRoom(room: GuiRoom<GameState>) {
         }
     });
 
-    // shelf
-    GenerateDropTarget(room, state.Desk, new Render.Box(300,150, 128,25));
+    switch(State.TimePeriod) {
+        case TimePeriod.Alchemy:
+            break;
+
+        case TimePeriod.Present:
+
+            GenerateDropTarget(room, State.Desk, new Render.Box(300,150, 128,25));
+
+            break;
+
+        case TimePeriod.Future:
+            break;
+    }
 
     // regenerate inventory
-    PopulateDropTarget(room, state.Inventory, state.InventoryBox);
+    PopulateDropTarget(room, State.Inventory, State.InventoryBox);
 };
 
 function ResetGame(room: GuiRoom<GameState>) {
@@ -139,7 +194,7 @@ function ResetGame(room: GuiRoom<GameState>) {
     let triggerBox = room.makeDummyObject("#000", 0, 0, "Trigger");
     room.onClick(triggerBox, clickedWith => {
         if(clickedWith == null) {
-            GenerateRoom(room);
+            PopupTimeMachine(room);
         }
     });
 
